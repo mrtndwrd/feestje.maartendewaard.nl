e727424949237a3534fbcdb706d4c7503cea1b43
Maarten de Waard <maarten@greenhost.nl>
2020-03-28 17:31:16 +0100

    add participants API

---
+++ api/roommembers.php
@@ -0,0 +1,99 @@
+<?php
+$servername = "maarte91.sql.ghserv.net";
+$username = "maartendewaa";
+$password = "yacyehigEndoccorExCyzeQuavirg8";
+$dbname = "maartendewaard_nl_housewarming";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// $sql = "SELECT * FROM jitsi";
+// $result = mysqli_query($conn, $sql);
+// 
+// if (mysqli_num_rows($result) > 0) {
+//     // output data of each row
+//     while($row = mysqli_fetch_assoc($result)) {
+//         echo "id: " . $row["id"]. " - room : " . $row["room"]. " - members: " . $row["members"]. "<br>";
+//     }
+// } else {
+//     echo "0 results";
+// }
+// 
+
+// For testing:
+// $_GET = ['function' => 'getmembers', 'room' => 'woonkamer'];
+// $_GET = ['function' => 'setmembers', 'room' => 'woonkamer'];
+// $_GET = ['function' => 'getrooms', 'room' => 'woonkamer'];
+
+if ($_GET['function'] == 'getmembers') {
+    $room = $_GET['room'];
+    getMembers($conn, $room);
+} elseif ($_GET['function'] == 'setmembers') {
+    $room = $_GET['room'];
+    $request_body = file_get_contents('php://input');
+
+    // Uncomment this for testing:
+    // $request_body = '{"members":{"ac3d9f4a":{"displayName":"erika","formattedDisplayName":"erika (me)"}}}';
+    $data = json_decode($request_body);
+    setMembers($conn, $room, $data->members);
+} elseif ($_GET['function'] == 'getrooms') {
+    getRooms($conn);
+} else {
+    echo "Function not known";
+}
+
+function setMembers($conn, $room, $members) {
+    $room = mysqli_real_escape_string($conn, strtolower($room));
+
+    $safe_members = [];
+    foreach ($members as $memberHash => $memberData) {
+        if (isset($memberData->displayName))
+            $safe_members[] = $memberData->displayName;
+        else 
+            $safe_members[] = $memberHash;
+        var_dump($safe_members);
+    }
+    $safe_members =  preg_replace('/[^a-zA-Z\d\s:]/', '', $safe_members);
+    $safe_members = json_encode($safe_members);
+    $query = "UPDATE jitsi SET members = '$safe_members' WHERE room = '$room'";
+    if (mysqli_query($conn, $query)) {
+        echo "success";
+    } else {
+        echo "setMembers failed";
+    }
+}
+
+function getMembers($conn, $room) {
+    $room = mysqli_real_escape_string($conn, strtolower($room));
+    $query = "SELECT members FROM jitsi WHERE room = '$room'";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) > 0) {
+        // output first data row
+        $row = mysqli_fetch_assoc($result);
+        echo $row['members'];
+    } else {
+        echo "['SOMETHING WENT WRONG']";
+    }
+
+}
+
+function getRooms($conn) {
+    $query = "SELECT room FROM jitsi";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) > 0) {
+        // output first data row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $rooms[] = $row['room'];
+        }
+        echo json_encode($rooms);
+    } else {
+        echo "['SOMETHING WENT WRONG']";
+    }
+
+}
+
+mysqli_close($conn);
